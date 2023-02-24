6023b020ee0bf55953e05da9f458dbbcb93a27b4
Jannis Weis <31143295+weisJ@users.noreply.github.com>
2023-02-24 17:49:57 +0100

    Collect inner states in subset construction as it might not be a vector

--- src/Deterministic.jl
+++ src/Deterministic.jl
@@ -34,8 +34,8 @@ mutable struct SubsetConstructionAutomaton{S,X} <: EpochStateAutomaton{MultiStat
     terminal_states::Vector{MultiState{X}}
 
     function SubsetConstructionAutomaton(A::AbstractAutomaton{S,X}) where {S,X}
-        inner_states::Vector{S} = states(A)
-        start = epsilon_closure(A, inner_states, initial(A))
+        inner_states::Vector{S} = collect(states(A))
+        start = epsilon_closure(A, inner_states, initial_states(A))
         B = new{S,X}(A, Vector{MultiState{X}}(), start, Vector{MultiState{X}}())
         _subset_construction(A, B, inner_states)
         return B
