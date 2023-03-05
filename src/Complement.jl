8547369b498963ee192de78ff5a3b2ada6ce4833
Jannis Weis <31143295+weisJ@users.noreply.github.com>
2023-03-05 23:14:20 +0100

    Add missing override of terminal_states for complement

--- src/Complement.jl
+++ src/Complement.jl
@@ -5,6 +5,8 @@ end
 
 wrappee(A::ComplementAutomaton{S,X}) where {S,X} = A.inner
 is_terminal(A::ComplementAutomaton{S,X}, state::S) where {S,X} = !is_terminal(wrappee(A), state)
+terminal_states(A::ComplementAutomaton{S,X}) where {S,X} = (s for s ∈ states(A) if is_terminal(A, s))
+
 
 function complement(A::AbstractAutomaton{S,X}) where {S,X}
     return ComplementAutomaton(A)
