dd179c547b4554457a11344dd42e534f2a74a7bb
Jannis Weis <31143295+weisJ@users.noreply.github.com>
2023-03-05 18:33:35 +0100

    Add test for detecting single edge self loops

--- test/Loop.jl
+++ test/Loop.jl
@@ -19,3 +19,15 @@
     CGT.add_edge!(A, s2, :a, s3)
     @test CGT.contains_loop_with_non_trivial_support(A)
 end
+
+@testset "Self loop is detected" begin
+    X = CGT.Alphabet([:a])
+    A = CGT.Automaton(X)
+    s1 = CGT.initial(A)
+    s2 = CGT.add_state!(A)
+
+    CGT.add_edge!(A, s1, :a, s2)
+    CGT.add_edge!(A, s2, :a, s2)
+
+    @test CGT.contains_loop_with_non_trivial_support(A)
+end
