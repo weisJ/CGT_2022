1130841d09b556ba36b401548f072bd93cd3a429
Jannis Weis <31143295+weisJ@users.noreply.github.com>
2023-03-06 11:19:20 +0100

    Add clarification about traversal order of StateIterator

--- src/StateIterator.jl
+++ src/StateIterator.jl
@@ -1,4 +1,8 @@
-# TODO: Document contract
+"""
+An abastract iterator for automatons, which traverses the states in a depth first fashion.
+States can be marked for the purpose of "traverse and mark" algorithms. These marks are only valid
+while the given iterator is the only active iterator.
+"""
 abstract type StateIterator{S} end
 
 """
@@ -26,6 +30,9 @@ is used as a callback and receives the currently looked at state.
 It returns either `Continue` or `Break` to continue down the graph or
 stop iteration at the current state.
 The `edge_filter` parameter can be used to filter the edges, which are traversed.
+
+Traversal should be done in a depth first fashion, where states are visited only once, except in the following situation:
+A state may be visited more than once if it aids in completing a loop such that the last edge of the loop is visited.
 """
 function traverse(A::AbstractAutomaton{S,X}, α::S, it::StateIterator{S}=state_iterator(A);
     enter::F1, exit::F2=s::S -> Continue, edge_filter::F3=(_, _) -> true) where {S,X,F1,F2,F3} end
